544159037362623d52ccc0851f5c889681902356
gustaxxcwb <gustavo.schuarca@escola.pr.gov.br>
2025-06-16 11:51:06 -0300

    Add files via upload

--- exercício1/script.js
+++ exercício1/script.js
@@ -1,17 +1,25 @@
+//função que vai somar os valores
 function somar(){
+//declaração de variáveis
     let num1 = 0;
     let num2 = 0;
 
+    //entrada de dados pelo usuário e atribuíção nas variáveis
      num1 = Number( window.prompt('Digite um número'));
      num2 = Number( window.prompt('Digite outro número'));
 
+    //a variável soma e recebe o resultado da soma das variáveis num1 e num2
     let soma = num1 + num2;
 
+    //mostra o resultado pro usuário
     alert(soma);
 
+    //verifica se o resultado da soma é maior ou menor que 10
     if(soma > 10) {
+        //caso seja maior que 10, mostra a frase abaixo pro usuário
         alert("O resultado é maior que 10");
     } else {
+        //se for menor que 10, mostra essa frase
         alert("Não é maior que 10");
     }
 }     
