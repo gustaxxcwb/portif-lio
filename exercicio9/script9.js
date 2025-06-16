f771f0077ce83f0776767ac7b25f27655013a96b
gustaxxcwb <gustavo.schuarca@escola.pr.gov.br>
2025-06-16 12:18:12 -0300

    Add files via upload

---
+++ exercicio9/script9.js
@@ -0,0 +1,19 @@
+let n1;
+let n2;
+
+
+function diferenca() {
+
+    n1 = window.prompt("Digite o primeiro número:");
+    n2 = window.prompt("Digite o segundo número:");
+
+    if (n1 > n2) {
+            alert("O 1° número é maior ")
+
+    } else {
+        alert("O 2° número é maior")
+    }
+
+
+
+}
